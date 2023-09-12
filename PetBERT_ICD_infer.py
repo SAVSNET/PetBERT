03b233938a68454e5f72f23e9ed6cc1cd5f49962
seanfarr788 <seanoliverfarrell@gmail.com>
2023-09-12 12:28:50 +0100

    Updated inference

--- PetBERT_ICD_infer.py
+++ PetBERT_ICD_infer.py
@@ -1,31 +1,38 @@
 from transformers import AutoConfig, Trainer, TextClassificationPipeline, AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding, TrainingArguments
 import pandas as pd
 from datasets import load_dataset
-import numpy as np 
+import numpy as np
 import torch.nn.functional as nnf
 import torch
 import argparse
 import glob
-from utils.training import compute_metrics, calculate_class_weights, multi_label_metrics
 
 argparser = argparse.ArgumentParser(description='Description of your program')
-argparser.add_argument('--model_pathway', help='Pretrained Model', default='test_1')
-argparser.add_argument('--data', help='Pretrained Model', default='test_1')
+argparser.add_argument(
+    '--model_pathway', help='Pretrained Model', default='models/PetBERT-ICD')
+argparser.add_argument('--data', help='Pretrained Model',
+                       default='data/PetBERT_ICD/raw/full_dataset.csv')
 argparser.add_argument('--threshold', help='Threshold', default=0.8)
 args = vars(argparser.parse_args())
 
+latest_checkpoint = glob.glob(args['model_pathway']+"/*")[-1]
 
-tokenizer= AutoTokenizer.from_pretrained(PATH, padding=True, truncation=True, max_length=512, problem_type="multi_label_classification")
-model= AutoModelForSequenceClassification.from_pretrained(str(PATH), local_files_only=True, problem_type="multi_label_classification", num_labels=20)
-config = AutoConfig.from_pretrained(PATH+'/config.json', local_files_only=True)
+tokenizer = AutoTokenizer.from_pretrained(
+    latest_checkpoint, padding=True, truncation=True, max_length=512, problem_type="multi_label_classification")
+model = AutoModelForSequenceClassification.from_pretrained(str(
+    latest_checkpoint), local_files_only=True, problem_type="multi_label_classification", num_labels=20)
+config = AutoConfig.from_pretrained(latest_checkpoint+'/config.json', local_files_only=True)
+
 classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer)
-data_file = "Datasets/full_dataset_split/all/"+args['data']+".csv"
-data_file = "hpcia_datasets/test_new.csv"
-datasets = load_dataset('csv', data_files={"Test":data_file}, delimiter=',', split='Test[:100%]')
-#datasets = datasets.rename_column("item_text", "text")
+
+
+datasets = load_dataset(
+    'csv', data_files={"Test": args['data']}, delimiter=',', split='Test[:100%]')
+
 
 def preprocess_function(examples):
     return tokenizer(examples['text'], padding=True, truncation=True, max_length=512)
+
 
 tokenized_dataset = datasets.map(preprocess_function, batched=True)
 
@@ -36,9 +43,9 @@ training_args = TrainingArguments(
     per_device_eval_batch_size=32,
     dataloader_num_workers=10,
     dataloader_pin_memory=True,
-    jit_mode_eval = True,
+    jit_mode_eval=True,
     bf16=True,
-            )
+)
 
 label_dictionary = {v: k for k, v in config.label2id.items()}
 
@@ -48,7 +55,7 @@ trainer = Trainer(
     args=training_args,
     eval_dataset=tokenized_dataset,
     tokenizer=tokenizer,
-    data_collator=data_collator, 
+    data_collator=data_collator,
 )
 
 predictions = trainer.predict(tokenized_dataset)
@@ -63,17 +70,15 @@ datasets = datasets.to_pandas()
 results = []
 threshold_val = float(args['threshold'])
 for narrative in probs.numpy():
-    value = np.where(narrative >=threshold_val)[0]
+    value = np.where(narrative >= threshold_val)[0]
     labels = [label_dictionary[i] for i in value]
     results.append(labels)
 
 
 predictions_array = np.zeros(predictions.predictions.shape)
-predictions_array[np.where(predictions.predictions >= 0.8)] = 1
+predictions_array[np.where(predictions.predictions >= args['threshold'])] = 1
 full_data = pd.DataFrame(predictions_array)
 full_data = full_data.rename(columns=dict(label_dictionary))
 full_data["ICD_11"] = results
 full_data = pd.concat([datasets, full_data], axis=1)
 full_data.to_csv("hpcia_datasets/test_new_icd.csv")
-#full_data.to_csv("Datasets/full_dataset_split/all/results/" + args['data'] + ".csv")
-
